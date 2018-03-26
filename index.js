cf21e3e06853c458f224540cd9e0c4d766054fed
Giavinh Lam <glam01@uoguelph.ca>
2018-03-26 00:39:46 -0400

    test

--- index.js
+++ index.js
@@ -14,27 +14,26 @@ http.listen(3000, function(){
 */
 
 //with socket.io library
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var clientCount = 0;
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/database', function(req, res) {
-  console.log('rip');
-  res.sendFile(__dirname + '/index.html');
-  //alert("hello");
- // console.log('You sent the name "' + req.body.user + '".');
-  // res.send(req.body.user);
-  //res.send
-});
+// app.post('/database', function(req, res) {
+//   console.log('rip');
+//   res.sendFile(__dirname + '/index.html');
+//   //alert("hello");
+//  // console.log('You sent the name "' + req.body.user + '".');
+//   // res.send(req.body.user);
+//   //res.send
+// });
 
 io.on('connection', function(socket){
   //console.log('a user connected');
