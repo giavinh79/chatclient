2c9a8d882ffdbbca5f58a2c4e8d33c85694e3ae8
Giavinh Lam <glam01@uoguelph.ca>
2018-03-26 20:47:28 -0400

    -a

--- index.js
+++ index.js
@@ -3,16 +3,28 @@
 // var io = require('socket.io')(http);
 // var clientCount = 0;
 
-const app = require('express')(); //dependencies/modules needed
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-var clientCount = 0;
+// const app = require('express')(); //dependencies/modules needed
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+// var clientCount = 0;
 
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/generic.html');
-  // res.sendFile(INDEX);
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/generic.html');
+//   // res.sendFile(INDEX);
+// });
+
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // app.get('/favicon.ico', function(req, res) {
 //   res.sendFile(__dirname + "/" + "favicon.ico");
