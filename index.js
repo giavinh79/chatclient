f65fb9e51ce592e01d8cb0519be8445dd44d6e2c
GV79 <glam01@uoguelph.ca>
2018-04-05 15:37:10 -0400

    password matching, modularization setup

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-// var app = require('express')(); //dependencies/modules needed
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// var clientCount = 0;
-
 const app = require('express')(); //dependencies and modules
 const http = require('http').Server(app);
-// const io = require('socket.io')(http);
 const io = require('socket.io')(http, { wsEngine: 'ws' });
 var clientCount = 0;
-
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/index.html');
-//   // res.sendFile(INDEX);
-// });
 
 const PORT = process.env.PORT || 8080; 
 //process.env.PORT: convention for Heroku -> if nothing in environ. var then port is 8080
 
 app
-  // .get((req, res) => res.sendFile(__dirname + "/index.html"))
+  //.get((req, res) => res.sendFile(__dirname + "/index.html"))
   .get('/', function(req, res) {
       res.sendFile(__dirname + '/index.html');
-    //   // res.sendFile(INDEX);
   })
   .get('/backgroundWhite-compressed.jpg', function(req, res) {
       res.sendFile(__dirname + "/backgroundWhite-compressed.jpg");
@@ -38,64 +26,37 @@ app
   .get('/favicon.ico', function(req, res) {
     res.sendFile(__dirname + "/favicon.ico");
   });
-  //.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// app.get('/backgroundWhite.jpg', function(req, res) {
-//   res.sendFile(__dirname + "/backgroundWhite.jpg");
-// });
+const bodyParser = require('body-parser'); // Necessary to get form data with Express
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/style.css', function(req, res) {
-//   res.sendFile(__dirname + "/style.css");
-// });
+app.post('/database', function(req, res) {
+  console.log(req.body.user);
+  console.log(req.body.pwd);
 
-// .listen(8080, () => console.log(`Listening on localhost:8080`));
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://root:magma3@ds127129.mlab.com:27129/chatclient";
 
-// const express = require('express');
-// const socketIO = require('socket.io');
-// const path = require('path');
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database connection successful!");
+    db.close();
+  });
+});
 
-// const PORT = process.env.PORT || 3000;
-// const INDEX = path.join(__dirname, 'index.html');
+app.post('/account', function(req, res) {
+  console.log(req.body.username);
+  console.log(req.body.password);
 
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://root:magma3@ds127129.mlab.com:27129/chatclient";
 
-// app.get('/favicon.ico', function(req, res) {
-//   res.sendFile(__dirname + "/" + "favicon.ico");
-// });
-
-// app.get('/backgroundWhite.jpg', function(req, res) {
-//   res.sendFile(__dirname + "/" + "backgroundWhite.jpg");
-// });
-
-// app.get('/style.css', function(req, res) {
-//   res.sendFile(__dirname + "/" + "style.css");
-// });
-
-// app.get('/loginstyle.css', function(req, res) {
-//   res.sendFile(__dirname + "/" + "loginstyle.css");
-// });
-
-// app.get('/loggedin.html', function(req, res) {
-//   res.sendFile(__dirname + "/" + "loggedin.html");
-// });
-
-// app.get('/style.css', function(req, res) {
-//   res.sendFile(__dirname + '/style.css');
-// });
-
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.post('/database', function(req, res) {
-//   console.log('rip');
-//   res.sendFile(__dirname + '/index.html');
-//   //alert("hello");
-//  // console.log('You sent the name "' + req.body.user + '".');
-//   // res.send(req.body.user);
-//   //res.send
-// });
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database connection successful!");
+    db.close();
+  });
+});
 
 io.on('connection', function(socket){
     clientCount++;
