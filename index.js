140a2e6d13dc4dbf67d4a7ba99d1792e75397729
GV79 <glam01@uoguelph.ca>
2018-04-06 12:20:55 -0400

    cleaned up some code, adding some mongoDB functionality

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/database', function(req, res) {
   console.log(req.body.pwd);
 
   var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://root:magma3@ds127129.mlab.com:27129/chatclient";
+  var url = "mongodb://defaultUser:chatpassword123@ds127129.mlab.com:27129/chatclient";
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
@@ -44,17 +44,22 @@ app.post('/database', function(req, res) {
   });
 });
 
-app.post('/account', function(req, res) {
+app.post('/signup', function(req, res) {
   console.log(req.body.username);
   console.log(req.body.password);
 
   var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://root:magma3@ds127129.mlab.com:27129/chatclient";
+  var url = "mongodb://defaultUser:chatpassword123@ds127129.mlab.com:27129/chatclient";
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log("Database connection successful!");
-    db.close();
+    var dbo = db.db("chatclient");
+    var myobj = { user: req.body.username, pass: req.body.password };
+    dbo.collection("accounts").insertOne(myobj, function(err, res) {
+     if (err) throw err;
+     console.log("1 document inserted.");
+     db.close();
+    });
   });
 });
 
@@ -69,38 +74,10 @@ io.on('connection', function(socket){
     });
 
     socket.on('chat message', function(msg){
-      //  if (msg != null)
        io.emit('chat message', msg); //for everyone
     });
 });
 
-// http.listen(8080, function(){
-//   console.log('listening on localhost:8080');
-// });
-
 http.listen(PORT, function(){
   console.log('listening on localhost:8080');
 });
-
-// const express = require('express');
-// const socketIO = require('socket.io');
-// const path = require('path');
-
-// const PORT = process.env.PORT || 3000;
-
-// const server = express()
-//   .use((req, res) => res.sendFile(__dirname + "/generic.html") )
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-// const io = socketIO(server);
-
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-// const app = require('express')(); //dependencies/modules needed
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-// var clientCount = 0;
-
-// http.listen(3000, function(){
-//   console.log('listening on localhost:3000');
-// });
