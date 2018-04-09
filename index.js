41613dbdabc070921887e3395061421073638182
GV79 <glam01@uoguelph.ca>
2018-04-08 21:43:44 -0400

    accounts implementedand some design changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('express')(); //dependencies and modules
 const http = require('http').Server(app);
 const io = require('socket.io')(http, { wsEngine: 'ws' });
+var fs = require('fs'); // bring in the file system api
+var mustache = require('mustache');
+var MongoClient = require('mongodb').MongoClient;
 var clientCount = 0;
 
 const PORT = process.env.PORT || 8080;
@@ -30,62 +33,56 @@ app
 const bodyParser = require('body-parser'); // Necessary to get form data with Express
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var url = "mongodb://default:chatpassword123@ds127129.mlab.com:27129/chatclient";
 app.post('/database', function(req, res) {
-  console.log(req.body.user);
-  console.log(req.body.pwd);
-
-  var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://default:chatpassword123@ds127129.mlab.com:27129/chatclient";
-
+  // var url = "mongodb://default:chatpassword123@ds127129.mlab.com:27129/chatclient";
   MongoClient.connect(url, function(err, db) {
     var dbo = db.db("chatclient");
     var query = { user: req.body.user };
     dbo.collection("accounts").find(query).toArray(function(err, result) {
       if (err) throw err;
-      if (result == null || result == "")
-      {
+      if (result == null || result == "") {
         console.log("none found");
-      }
-      else
-      {
+      } else {
         console.log(result);
-        if (result[0].pass == req.body.pwd)
-        {
+        if (result[0].pass == req.body.pwd) {
           console.log("Successful login");
-        }
-        else
-        {
+
+          var account = {
+            accountname: req.body.user
+          };
+
+          fs.readFile(__dirname + '/loggedin.html', 'utf8', (err, data) => {
+            if (err) throw err;
+            var html = mustache.to_html(data, account); //template system used to generate dynamic HTML
+            res.send(html);
+          });
+
+        } else {
           console.log("Failed login");
         }
       }
-
       db.close();
     });
   });
 });
 
 app.post('/signup', function(req, res) {
-  console.log(req.body.username);
-  console.log(req.body.password);
-
-  var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://default:chatpassword123@ds127129.mlab.com:27129/chatclient";
+  // var url = "mongodb://default:chatpassword123@ds127129.mlab.com:27129/chatclient";
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("chatclient");
-
+  
     var query = { user: req.body.user };
     dbo.collection("accounts").find(query).toArray(function(err, result) {
       if (err) throw err;
-      if (!(result == null && result == ""))
-      {
-        console.log("account already exists: fail")
-      }
-      else
-      {
+      if (!(result == null && result == "")) {
+        console.log("account already exists: fail");
+      } else {
         var myobj = { user: req.body.username, pass: req.body.password };
-        dbo.collection("accounts").insertOne(myobj, function(err, res) {
+        dbo.collection("accounts").insertOne(myobj, function(err, res) 
+        {
           if (err) throw err;
           console.log("1 document inserted.");
           db.close();
